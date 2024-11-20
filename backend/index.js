72f66168d7d0ea042adfaeb3cc0d6a4173eae0e6
DevPrahlad <prahlad.singh.education@gmail.com>
2024-11-20 11:24:56 +0530

    Updated Cloudinary configuration and modified upload routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+import { fileURLToPath } from "url";
 import path from "path";
 
 // Files
@@ -17,12 +19,20 @@ connectDB();
 
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://moviedb-ujxw.vercel.app/", // Replace with your frontend URL
+    credentials: true,
+  })
+);
 
-const PORT = process.env.PORT || 3000;
+// Define __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Routes
 app.use("/api/v1/users", userRoutes);
@@ -30,7 +40,10 @@ app.use("/api/v1/genre", genreRoutes);
 app.use("/api/v1/movies", moviesRoutes);
 app.use("/api/v1/upload", uploadRoutes);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+// Catch-all route for undefined API endpoints
+app.use((req, res) => {
+  res.status(404).send({ message: "API route not found" });
+});
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Export the app for Vercel
+export default app;
